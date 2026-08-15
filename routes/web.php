cba4bbb7d51c0d316da94a2a9e7d55bbd1200cd5
sukradithama <sukra.adithama@gmail.com>
2026-08-15 15:22:04 +0800

    feat: add basic application routes

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,10 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/request', function () {
+    return 'Request Page';
+});
+
+Route::get('/dashboard', function () {
+    return 'Dashboard Page';
 });
