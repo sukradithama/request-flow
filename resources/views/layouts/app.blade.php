8bf73e2cae5bb1b7749fab0a4f1131970a4e0fef
sukradithama <sukra.adithama@gmail.com>
2026-08-22 23:12:02 +0800

    feat: change navigasi bar link name

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -18,10 +18,9 @@
             </button>
             <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
                 <div class="navbar-nav">
-                    <a class="nav-link active" aria-current="page" href="#">Home</a>
-                    <a class="nav-link" href="#">Features</a>
-                    <a class="nav-link" href="#">Pricing</a>
-                    <a class="nav-link disabled" aria-disabled="true">Disabled</a>
+                    <a class="nav-link" href="#">Request</a>
+                    <a class="nav-link" href="#">Category</a>
+                    <a class="nav-link" href="#">User</a>
                 </div>
             </div>
         </div>
