b3b4087eb47c9f0e1f88c8b3b0502315988f2c86
sukradithama <sukra.adithama@gmail.com>
2026-08-22 13:01:42 +0800

    feat: update routes with new blade structure

--- routes/web.php
+++ routes/web.php
@@ -2,10 +2,6 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/request', function () {
-    return view("request");
-});
-
-Route::get('/dashboard', function () {
-    return view('dashboard');
+Route::get('/', function () {
+    return view("request.index");
 });
