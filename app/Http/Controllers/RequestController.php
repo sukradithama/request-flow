bb23ccbff6f6d513a9eec43b34e259d3cd48d9fd
sukradithama <sukra.adithama@gmail.com>
2026-08-22 23:20:36 +0800

    feat: implement index, create, and store request controller

---
+++ app/Http/Controllers/RequestController.php
@@ -0,0 +1,91 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Category;
+use App\Models\Request as RequestModel;
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Str;
+
+class RequestController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $requests=RequestModel::get();
+        return view('request.index',compact('requests'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        $categories = Category::where('is_active', true)->get();
+        $users = User::where('is_active', true)->get();
+        return view('request.create', compact('categories', 'users'));
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        $validated = $request->validate([
+            'category_id' => 'required',
+            'user_id' => 'required',
+            'assigned_to' => 'nullable',
+            'title' => 'required|string|max:255',
+            'description' => 'required|string',
+            'priority' => 'required|in:low,medium,high,critical',
+        ]);
+        $newRequest = RequestModel::create([
+            'category_id' => $validated['category_id'],
+            'user_id' => $validated['user_id'],
+            'assigned_to' => $validated['assigned_to'] ?? null,
+            'title' => $validated['title'],
+            'description' => $validated['description'],
+            'status' => 'pending',
+            'priority' => $validated['priority'],
+            'slug' => Str::slug($validated['title']) . '-' . uniqid(),
+        ]);
+        return redirect()
+            ->route('IndexRequest')
+            ->with('success', 'Request berhasil dibuat.');
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(string $id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        //
+    }
+}
