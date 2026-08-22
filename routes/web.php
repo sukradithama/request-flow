42de2b8a34483ed2689b6f65a8ec92337558bd27
sukradithama <sukra.adithama@gmail.com>
2026-08-22 23:07:47 +0800

    feat: add Request route

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,14 @@
 <?php
 
+use App\Http\Controllers\RequestController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view("request.index");
-});
+// Request
+Route::get('/request',[RequestController::class,'index'])->name('IndexRequest');
+Route::get('/request/create',[RequestController::class,'create'])->name('CreateRequest');
+Route::post('/request',[RequestController::class,'store'])->name('StoreRequest');
+Route::get('/request/{slug}',[RequestController::class,'show'])->name('ShowRequest');
+Route::get('/request/{slug}/edit',[RequestController::class,'edit'])->name('EditRequest');
+Route::put('/request/{slug}',[RequestController::class,'update'])->name('UpdateRequest');
+Route::delete('/request/{slug}',[RequestController::class,'destroy'])->name('DeleteRequest');
+
